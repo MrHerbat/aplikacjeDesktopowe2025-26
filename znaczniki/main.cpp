fb063088171d4a7d6972e887cc08be6d5dd94346
Maciej <113918343+MrHerbat@users.noreply.github.com>
2025-09-04 12:40:19 +0200

    Add files via upload

--- znaczniki/main.cpp
+++ znaczniki/main.cpp
@@ -33,6 +33,45 @@ int main()
 
     int tab[10];
     for(int i = 0;i<10;i++){
-        tab[i]=rand()+10;
+        tab[i]=(rand()%(100-10))+10;
+        cout<<tab[i]<<" ";
+    }
+    cout<<endl;
+    int *w3;
+    w3=&tab[0];
+    cout<<&w3<<endl;
+    for(int i = 0; i<10;i++){
+        *(w3+i)=(rand()%(1000-100))+100;
+        cout<<*(w3+i)<<" ";
+    }
+    cout<<endl;
+    for(int i = 0;i<10;i++){
+        cout<<tab[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"========="<<endl;
+
+    int *dTab = new int[20];
+    for(int i = 0; i<20; i++){
+        dTab[i]=rand()%10;
+        cout<<dTab[i]<<" ";
+    }
+    cout<<endl;
+    delete dTab;
+    for(int i = 0; i<20; i++){
+        cout<<dTab[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"========="<<endl;
+    int **dTab2 = new int*[6];
+    for(int i = 0; i<6;i++){
+        dTab2[i] = new int[3];
+    }
+    for(int i=0;i<6;i++){
+        for(int j=0;j<3;j++){
+            dTab2[i][j]=(rand()%90)+10;
+            cout<<dTab2[i][j]<<" ";
+        }
+        cout<<endl;
     }
 }
